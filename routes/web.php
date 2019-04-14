3f24e8e0b334366c64f1db15b1803ceeb7997532
Bret Blackford <bret_blackford@yahoo.com>
2019-04-14 14:51:52 -0500

    working om test Db

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,36 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bret', function() {
+    return 'Welcome from Bret';
+});
+
+
+Route::get('/debug', function() {
+    $debug = ['Environment' => App::environment(),];
+
+    $debug['MySQL connection config'] = config('database.connections.mysql');
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $ex) {
+        $debug['DATABASE CONNECTION TEST'] = 'failed: ' . $ex->getMessage();
+    }
+    dump($debug);
+});
+
