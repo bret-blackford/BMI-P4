92e1e6be22f72c2d0e226e2da8248e5352888fa2
Bret Blackford <bret_blackford@yahoo.com>
2019-04-14 19:17:56 -0500

    added Db columns to table *patient*

---
+++ database/migrations/2019_04_15_000240_create_patient_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            
+            $table->string('name');
+            $table->date('birthdate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient');
+    }
+}
